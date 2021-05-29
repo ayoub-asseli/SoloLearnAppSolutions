0aef120526a98fcf47269f320503c27fce4b36c4
ayoub-asseli <59586260+ayoub-asseli@users.noreply.github.com>
2021-05-30 00:11:16 +0200

    Update That'sOdd.cs

--- Medium/MyC#Solutions/That'sOdd.cs
+++ Medium/MyC#Solutions/That'sOdd.cs
@@ -4,10 +4,10 @@ class Program {
     static void Main(string[] args) {
     	int n = Int32.Parse(Console.ReadLine());
     	int s = 0;
-    	for (int i = O; i < n; i++){
+    	for (int i = 0; i < n; i++){
     		int nb = Int32.Parse(Console.ReadLine());
     		if (nb % 2 == 0) 
-    			s += nb
+    			s += nb;
     	}
     	Console.WriteLine(s);
     }
